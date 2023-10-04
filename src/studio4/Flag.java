b26e0966164250043024c150ebbd812d4b769a22
paoladiaz <paoladiaz@localhost>
2023-10-04 15:48:49 -0500

    Finished to individual flag
    Signed-off-by: paoladiaz <paoladiaz@localhost>

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,35 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		StdDraw.setPenColor(141,159,190); // cute grey blue 
+		
+		StdDraw.filledRectangle(0.5, 0.5, .4, .3);	
+		
+		StdDraw.setPenColor(144,225,245);
+		
+		double [] x = {.3, .6, .45, .3 };
+		double[] y = {.5,.5,.3, .4};
+		StdDraw.filledPolygon(x,y);
+		
+		double [] x2 = {.4, .6, .5};
+		double[] y2 = {.5,.5,.6};
+		StdDraw.filledPolygon(x2,y2);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,77 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		//What shape
+		String shapeType = in.next();
+		
+		
+		//what color
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		
+		StdDraw.setPenColor(redComponent, greenComponent , blueComponent);
+		
+		// is the shape filled 
+		boolean isFilled = in.nextBoolean();
+		
+	if (shapeType.equals("triangle"))	
+	{
+		double x1 = in.nextDouble();
+		double y1 = in.nextDouble();
+		double x2 = in.nextDouble();
+		double y2 = in.nextDouble();
+		double x3 = in.nextDouble();
+		double y3 = in.nextDouble();
+		
+		if(isFilled)
+		{
+			double[] x = {x1, x2, x3};
+			double [] y = {y1, y2, y3};
+		    StdDraw.filledPolygon(x, y);
+		}else
+		{
+			
+			StdDraw.line(x1,y1,x2,y2);
+	        StdDraw.line(x2, y2, x3,y3);
+	        StdDraw.line(x3, y3, x1, y1);
+		}
+	}
+	else
+	{
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		
+		if (shapeType.equals("ellipse"))
+		{
+			if (isFilled)
+			{
+				
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+			}else
+			{
+				
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+		}
+		
+		if (shapeType.equals("rectangle"))
+		{ 
+			if (isFilled)
+			{
+				
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}else
+			{
+				
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		
+		}
+		
+	}
 	}
 }
